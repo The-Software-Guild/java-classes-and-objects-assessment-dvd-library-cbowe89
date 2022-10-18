36855f7877942747ee8291c388fa9e1789739b69
Courtney Bowe <csmyers89@gmail.com>
2022-10-18 10:36:32 -0500

    Dvd.java - changed variables to camelCase, implemented setter methods

--- src/main/java/dto/Dvd.java
+++ src/main/java/dto/Dvd.java
@@ -3,53 +3,58 @@ package dto;
 import java.util.Date;
 
 public class Dvd {
-    private String title;
-    private String ReleaseDate;
-    private String MPAA;
-    private String DirectorsName;
-    private String Studio;
-    private String UserRating;
+    private String dvdTitle;
+    private String releaseDate;
+    private String mpaa;
+    private String directorsName;
+    private String studio;
+    private String userRating;
 
     public Dvd(String title) {
-        this.title = title;
+        this.dvdTitle = title;
     }
 
     public String getTitle() {
-        return title;
+        return dvdTitle;
     }
 
     public void setReleaseDate(String newReleaseDate) {
+        this.releaseDate = newReleaseDate;
     }
 
     public String getReleaseDate() {
-        return ReleaseDate;
+        return releaseDate;
     }
 
     public void setMPAA(String newMpaaRating) {
+        this.mpaa = newMpaaRating;
     }
 
     public String getMPAA() {
-        return MPAA;
+        return mpaa;
     }
 
     public void setDirectorsName(String newDirectorName) {
+        this.directorsName = newDirectorName;
     }
 
     public String getDirectorsName() {
-        return DirectorsName;
-    }
-
-    public void setUserRating(String newUserRating) {
-    }
-
-    public String getStudio() {
-        return Studio;
+        return directorsName;
     }
 
     public void setStudio(String newStudioName) {
+        this.studio = newStudioName;
+    }
+
+    public String getStudio() {
+        return studio;
+    }
+
+    public void setUserRating(String newUserRating) {
+        this.userRating = newUserRating;
     }
 
     public String getUserRating() {
-        return UserRating;
+        return userRating;
     }
 }
