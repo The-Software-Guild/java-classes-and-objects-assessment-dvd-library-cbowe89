9e3e531ba16b1f7597cbded2c2bf89ff92eb8428
Courtney Bowe <csmyers89@gmail.com>
2022-10-18 09:11:22 -0500

    Added template for App.java, DvdTest.txt file

---
+++ src/main/java/App.java
@@ -0,0 +1,20 @@
+import controller.DvdLibraryController;
+import dao.DvdLibraryDaoFileImpl;
+import dao.DvdLibraryDao;
+import ui.DvdLibraryView;
+import ui.UserIO;
+import ui.UserIOConsoleImpl;
+
+/**
+ *
+ * @author salajrawi
+ */
+public class App {
+
+    public static void main(String[] args) {
+        /*
+        Instantiate the UserIO, DvdLibraryView,dvdLibraryDao,  DvdLibraryController class and call the run method
+         */
+
+    }
+}
