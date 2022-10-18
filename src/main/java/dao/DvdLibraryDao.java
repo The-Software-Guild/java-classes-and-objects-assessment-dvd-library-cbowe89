4c9f3f010ead5476003da332352fc962df379993
Courtney Bowe <csmyers89@gmail.com>
2022-10-18 09:44:57 -0500

    DvdLibraryDao.java - added @throws to javadoc comments, added javadoc comments for editMPAA, editDirectorName, editUserRating, editStudio

--- src/main/java/dao/DvdLibraryDao.java
+++ src/main/java/dao/DvdLibraryDao.java
@@ -14,6 +14,7 @@ public interface DvdLibraryDao {
      * @param dvd dvd to be added to the library
      * @return the dvd object previously associated with the given
      * title if it exists, null otherwise
+     * @throws DvdLibraryDaoException prints msg
      */
     Dvd addDvd(String title, Dvd dvd) throws DvdLibraryDaoException;
 
@@ -21,6 +22,7 @@ public interface DvdLibraryDao {
      * Returns a List of all dvds in the library.
      *
      * @return List containing all dvds in the library.
+     * @throws DvdLibraryDaoException prints msg
      */
     List<Dvd> getAllDvds() throws DvdLibraryDaoException;
 
@@ -31,6 +33,7 @@ public interface DvdLibraryDao {
      * @param title title of the dvd to retrieve
      * @return the dvd object associated with the given title,
      * null if no such dvd exists
+     * @throws DvdLibraryDaoException prints msg
      */
     Dvd getDvd(String title) throws DvdLibraryDaoException;
 
@@ -42,24 +45,70 @@ public interface DvdLibraryDao {
      * @param title title of dvd to be removed
      * @return dvd object that was removed or null if no dvd
      * was associated with the given title
+     * @throws DvdLibraryDaoException prints msg
      */
     Dvd removeDvd(String title) throws DvdLibraryDaoException;
     /**
-     * Edits the dvd associated with the title.
+     * Edits the release date of dvd associated with the title.
      * Returns the dvd object that is being edited or null if
      * there is no dvd associated with the title
      *
      * @param title title of dvd to be edited
      * @return dvd object that was edited or null if no dvd
      * was associated with the given title
+     * @throws DvdLibraryDaoException prints msg
      */
     Dvd editReleaseDate(String title, String newReleaseDate) throws DvdLibraryDaoException;
 
+    /**
+     * Edits the MPAA rating of the dvd associated with the title.
+     * Returns the dvd object that is being edited or null if
+     * there is no dvd associated with the title.
+     *
+     * @param title title of dvd to be edited
+     * @param newMpaaRating rating of dvd
+     * @return dvd object that was edited or null if no dvd
+     * was associated with the given title
+     * @throws DvdLibraryDaoException prints msg
+     */
     Dvd editMPAA(String title, String newMpaaRating) throws DvdLibraryDaoException;
 
+    /**
+     * Edits the director name of the dvd associated with the title.
+     * Returns the dvd object that is being edited or null if
+     * there is no dvd associated with the title.
+     *
+     * @param title title of dvd to be edited
+     * @param newDirectorName name of film director
+     * @return dvd object that was edited or null if no dvd
+     * was associated with the given title
+     * @throws DvdLibraryDaoException prints msg
+     */
     Dvd editDirectorName(String title, String newDirectorName) throws DvdLibraryDaoException;
 
+    /**
+     * Edits the user rating of the dvd associated with the title.
+     * Returns the dvd object that is being edited or null if
+     * there is no dvd associated with the title.
+     *
+     * @param title of dvd to be edited
+     * @param newUserRating user rating of dvd
+     * @return dvd object that was edited or null if no dvd
+     * was associated with the given title
+     * @throws DvdLibraryDaoException prints msg
+     */
     Dvd editUserRating(String title, String newUserRating) throws DvdLibraryDaoException;
 
+    /**
+     * Edits the studio of the dvd associated with the title.
+     * Returns the dvd object that is being edited or null if
+     * there is no dvd associated with the title.
+     *
+     * @param title of dvd to be edited
+     * @param newStudioName studio that released film
+     * @return dvd object that was edited or null if no dvd
+     * was associated with the give title
+     * @throws DvdLibraryDaoException prints msg
+     */
     Dvd editStudio(String title, String newStudioName) throws DvdLibraryDaoException;
 }
