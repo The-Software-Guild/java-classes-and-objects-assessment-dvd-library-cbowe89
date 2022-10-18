7be00b0b974b4dc2fae16e18adb80a350036b437
Courtney Bowe <csmyers89@gmail.com>
2022-10-18 14:20:42 -0500

    Added comments

--- src/main/java/com/cbowe/dvdlibrary/App.java
+++ src/main/java/com/cbowe/dvdlibrary/App.java
@@ -1,8 +1,11 @@
 package com.cbowe.dvdlibrary;
 
+// Import DvdLibraryController from controller package
 import com.cbowe.dvdlibrary.controller.DvdLibraryController;
-import com.cbowe.dvdlibrary.dao.DvdLibraryDaoFileImpl;
+// Import DvdLibraryDao and DvdLibraryDaoFileImpl from dao package
 import com.cbowe.dvdlibrary.dao.DvdLibraryDao;
+import com.cbowe.dvdlibrary.dao.DvdLibraryDaoFileImpl;
+// Import DvdLibraryView, UserIO, and UserIOConsoleImpl from ui package
 import com.cbowe.dvdlibrary.ui.DvdLibraryView;
 import com.cbowe.dvdlibrary.ui.UserIO;
 import com.cbowe.dvdlibrary.ui.UserIOConsoleImpl;
@@ -10,7 +13,8 @@ import com.cbowe.dvdlibrary.ui.UserIOConsoleImpl;
 public class App {
 
     public static void main(String[] args) {
-        // Instantiate the UserIO, DvdLibraryView, DvdLibraryDao, and DvdLibraryController
+        // Instantiate UserIOConsoleImpl, DvdLibraryView,
+        // DvdLibraryDaoFileImpl, and DvdLibraryController objects
         UserIO myIo = new UserIOConsoleImpl();
         DvdLibraryView myView = new DvdLibraryView(myIo);
         DvdLibraryDao myDao = new DvdLibraryDaoFileImpl();
