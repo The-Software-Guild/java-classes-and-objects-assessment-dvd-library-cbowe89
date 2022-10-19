6614fd17f35af0d569662db9732522103fc26d3d
Courtney Bowe <csmyers89@gmail.com>
2022-10-18 23:56:58 -0500

    Added comments

--- src/main/java/com/cbowe/dvdlibrary/dao/DvdLibraryDaoException.java
+++ src/main/java/com/cbowe/dvdlibrary/dao/DvdLibraryDaoException.java
@@ -1,5 +1,10 @@
 package com.cbowe.dvdlibrary.dao;
 
+/**
+ * The {@code DvdLibraryDaoException} class extends Exception
+ * and creates specific exceptions for the DVDLibrary application.
+ */
+
 public class DvdLibraryDaoException extends Exception {
     public DvdLibraryDaoException(String message) {
         super(message);
